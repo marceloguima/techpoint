b677aec9b6cc279b712efc8754127a6af58cfba8
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-03-23 18:53:29 -0400

    carrinho de compras criado, no momento apenas abrindo e fechando

--- js/carrinho.js
+++ js/carrinho.js
@@ -3,3 +3,7 @@ const modalCarrinho = document.querySelector(".modal-carrinho")
 function mostraCarrinho(){
    modalCarrinho.style.display = "block"
 }
+
+function fecharCarrinho(){
+    modalCarrinho.style.display = "none"
+}
